9dc8e36298980dc5279fd932f35c0e14b9fe92a7
Yichen Lu <yi.lu-8@student.manchester.ac.uk>
2016-02-05 11:18:11 -0500

    done CAS

---
+++ Authenticator.py
@@ -0,0 +1,198 @@
+# This class will enable the developer to authenticate that their users
+# have a valid University of Manchester account.
+
+# @author Yichen Lu (yichen.lu529@gmail.com)
+# Transfer from Iain Hart's (iain@cs.man.ac.uk) PHP code into Python
+# @date 2nd Feb 2016
+
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+import uuid
+import logging
+import sys
+import time
+
+# program requiring authentication.
+DEVELOPER_URL = "http://178.62.125.116:5000/home"
+
+# Define the location of the service on the Computer Science server.
+AUTHENTICATION_SERVICE_URL = "http://studentnet.cs.manchester.ac.uk/authenticate/"
+
+# Define the location of CAS's logtout service on the Computer Science server.
+AUTHENTICATION_LOGOUT_URL = "http://studentnet.cs.manchester.ac.uk/systemlogout.php"
+
+class Authenticator(object):
+
+    def __init__(self, studylevel=False):
+        super(Authenticator, self).__init__()
+        self.studylevel = studylevel
+
+
+    def validateUser(self):
+        """
+        A static function to validate that a user has a University of Manchester
+        account. If the user is not authenticated the program will exit.
+        """
+        # If the user is already authenticated return.
+        if self.isAuthenticated():
+            logging.warning("----- isAuthenticated -----")
+            return True
+
+        # Else if the GET parameter csticket is empty this is a new user who
+        # we need to send for authentication.
+        elif not request.args.get('csticket'):
+            logging.warning("----- 1 -----")
+            self.sendForAuthentication()
+
+        # Else if the GET parameter csticket is populated but doesn't match
+        # the session csticket send the user for authentication.
+        elif request.args.get('csticket')!= session['csticket']:
+            self.sendForAuthentication()
+
+        else:
+            self.recordAuthenticatedUser()
+
+
+    def isAuthenticated(self):
+        """
+        A static function to determine whether a user is already authenticated.
+        @return boolean (true if authenticated, false if not)
+        """
+        # When a user is authenticated the session["authenticated"] is
+        # populated with a timestamp. If a numerical value is held return true.
+        authenticatedtimestamp = self.getTimeAuthenticated()
+
+        if len(authenticatedtimestamp)!=0 and authenticatedtimestamp.isdigit():
+            return True
+        else:
+            return False
+
+
+    # A static function to send a user to the authentication service.
+    def sendForAuthentication(self):
+        logging.warning("----- 2 -----")
+        # Generate a unique ticket.
+        csticket = str(uuid.uuid1())
+
+        # Save the ticket so we can confirm the same user is returning from
+        # the authentication service.
+        session["csticket"] = csticket
+
+        # Send the user to the School of Computer Science's server to validate.
+        # Append to the url the GET parameters 'url' which tells the
+        # authentication service where to return and append the csticket which
+        # will be used to confirm that the same user is returning.
+        url = AUTHENTICATION_SERVICE_URL+"?url="+DEVELOPER_URL+"&csticket="+csticket
+
+        # See if the developer requires the study level of the user.
+        if (self.studylevel):
+            url += "&studylevel=true"
+
+        logging.warning(url)
+        redirect(url)
+        # sys.exit() # TODO: is this correct?
+        # Respons(status=500)
+
+
+
+    # A static function to call if the developer requires the user's level
+    # of study. Note that this must be called before validateUser()
+    def requireStudyLevel(self):
+        self.studylevel = True
+
+
+
+    # A static function to reject a user who has failed to authenticate.
+    def rejectUser():
+        logging.warning("----- reject -----")
+        # sys.exit("Authentication failure")
+        redirect("http://178.62.125.116:5000")
+
+
+
+
+    # A static function to record that a user is authenticated.
+    def recordAuthenticatedUser():
+        # Record the time authenticated.
+        session["authenticated"] = int(time.time())
+
+        # Record the user's username.
+        session["username"] = request.args["username"]
+
+        # Record the user's full name.
+        session["fullname"] = request.args["fullname"]
+
+        # Record the users's category.
+        session["usercategory"] = request.args["usercategory"]
+
+        # Record the users's department.
+        session["department"] = request.args["dept"]
+
+        # Record the users's study level. Note this is only available
+        # if the developer has called requireStudyLevel(). TODO: studylevel
+        # if (isset(request.args["studylevel"]))
+        #     session["studylevel"] = request.args["studylevel"]
+
+
+
+    # A static function to get the timestamp when the user authenticated.
+    # @return string
+    def getTimeAuthenticated(self):
+        if session.get("authenticated"):
+            return session["authenticated"]
+        else:
+            return "haha"
+
+
+
+
+
+    # A static function to get the user's username as returned by the
+    # authentication service.
+    # @return string
+    def getUsername(self):
+        return session["username"]
+
+
+
+    # A static function to get the user's department as returned by the
+    # authentication service.
+    # @return string
+    def getUserDepartment(self):
+        return session["department"]
+
+
+
+    # A static function to get the user's year of study if the developer
+    # has called requireStudyLevel() before validating the user.
+    # @return string
+    def getStudyLevel(self):
+        if len(session["studylevel"])==0:
+            return "Unknown"
+        return session["studylevel"]
+
+
+
+
+    # A static function to get the user's full name as returned by the
+    # authentication service.
+    # @return string
+    def getFullName(self):
+        return session["fullname"]
+
+
+
+    # A static function to invalidate a user. This function will remove the
+    # data from the global variable session
+    def invalidateUser(self):
+        session.clear()
+        redirect(AUTHENTICATION_LOGOUT_URL)
+
+
+
+    # A static function to get the user's category. Options are staff, student,
+    # unknown.
+    # @return string
+    def getUserCategory():
+        return session["usercategory"]
+
+
--- flaskr.py
+++ flaskr.py
@@ -5,9 +5,13 @@ from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 from contextlib import closing
 import logging
+from werkzeug import secure_filename # for file upload
+import uuid # for CAS
+import time
 
-
-# create our little application :)
+UPLOAD_FOLDER = '/files'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+# create our little application
 app = Flask(__name__)
 
 # Load default config and override config from an environment variable
@@ -19,7 +23,6 @@ app.config.update(dict(
     PASSWORD='admin'
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
-
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
@@ -47,9 +50,8 @@ def teardown_request(exception):
 @app.route('/')
 # show_entries() <--- old class name
 def show_root():
-    # cur = g.db.execute('select title, text from entries order by id desc')
-    # entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
-    # return render_template('home.html', entries=entries)
+    if isAuthenticated():
+        return redirect(url_for('show_home'))
     cur = g.db.execute('SELECT content,url FROM contents WHERE url="/home";')
     content = [dict(text=row[0], url=row[1]) for row in cur.fetchall()]
 
@@ -60,12 +62,19 @@ def show_root():
 
 @app.route('/home')
 def show_home():
+
+    if session.get('username'):
+        username = session['fullname']
+    else:
+        username = "SoooooSad"
+
     cur = g.db.execute('SELECT content,url FROM contents WHERE url="/home";')
     content = [dict(text=row[0], url=row[1]) for row in cur.fetchall()]
 
     cur = g.db.execute('SELECT item,id FROM sidepanel ORDER BY id DESC;')
     sideitem = [dict(item=row[0], id=row[1]) for row in cur.fetchall()]
-    return render_template('home.html', content=content, sideitem=sideitem, username=app.config['USERNAME'])
+
+    return render_template('home.html', content=content, sideitem=sideitem, username=username)
 
 
 @app.route('/add', methods=['POST'])
@@ -86,8 +95,8 @@ def update_content():
     # if request.method == 'GET':
     logContent = request.args['content']
     logUrl = request.args['url']
-    logging.warning('update content -------- %s', logContent)
-    logging.warning('update URL -------- %s', logUrl)
+    logging.info('---------- update content -------- %s', logContent)
+    logging.info('---------- update URL -------- %s', logUrl)
     g.db.execute('UPDATE contents SET content = ? WHERE url = ?;',
                 [logContent, logUrl])
     g.db.commit()
@@ -101,7 +110,7 @@ def create_side():
     # if request.method == 'GET':
     logItem = request.args['item']
 
-    logging.warning('update item -------- %s', logItem)
+    logging.info('---------- update item -------- %s', logItem)
 
     g.db.execute('INSERT INTO sidepanel (item) VALUES (?);',
                 [logItem])
@@ -116,8 +125,8 @@ def update_side():
     # if request.method == 'GET':
     logItem = request.args['item']
     logID = request.args['id']
-    logging.warning('update item -------- %s', logItem)
-    logging.warning('update id -------- %s', logID)
+    logging.info('---------- update item -------- %s', logItem)
+    logging.info('---------- update id -------- %s', logID)
     g.db.execute('UPDATE sidepanel SET item = ? WHERE id = ?;',
                 [logItem, logID])
     g.db.commit()
@@ -131,7 +140,7 @@ def delete_side():
         abort(401)
     # if request.method == 'GET':
     logID = request.args['id']
-    logging.warning('update content -------- %s', logID)
+    logging.info('---------- update content -------- %s', logID)
     g.db.execute('DELETE FROM sidepanel WHERE id = ?;',
                 [logID])
     g.db.commit()
@@ -139,27 +148,132 @@ def delete_side():
     return redirect(url_for('show_home'))
 
 
+
+# program requiring authentication.
+DEVELOPER_URL = "http://178.62.125.116:5000/login"
+
+# Define the location of the service on the Computer Science server.
+AUTHENTICATION_SERVICE_URL = "http://studentnet.cs.manchester.ac.uk/authenticate/"
+
+# Define the location of CAS's logtout service on the Computer Science server.
+AUTHENTICATION_LOGOUT_URL = "http://studentnet.cs.manchester.ac.uk/systemlogout.php"
+
+studylevel = False
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_home'))
-    return render_template('login-n.html', error=error)
+    # Validate the user.
+    # If the user is already authenticated return.
+    if isAuthenticated():
+        logging.warning("----- isAuthenticated -----")
+        session['logged_in'] = True
+        # recordAuthenticatedUser()
+        return redirect(url_for('show_home'))
 
+    # Else if the GET parameter csticket is empty this is a new user who
+    # we need to send for authentication.
+    elif not request.args.get('csticket'):
+        logging.warning("----- 1 -----")
+        url = sendForAuthentication()
+        return redirect(url)
+
+    # Else if the GET parameter csticket is populated but doesn't match
+    # the session csticket send the user for authentication.
+    elif request.args.get('csticket')!= session['csticket']:
+        logging.warning("----- 3 -----")
+        url = sendForAuthentication()
+        return redirect(url)
+
+    else:
+        recordAuthenticatedUser()
+        session['logged_in'] = True
+        return redirect(url_for('show_home'))
+
+
+def isAuthenticated():
+    """
+    A static function to determine whether a user is already authenticated.
+    @return boolean (true if authenticated, false if not)
+    """
+    # When a user is authenticated the session["authenticated"] is
+    # populated with a timestamp. If a numerical value is held return true.
+    authenticatedtimestamp = getTimeAuthenticated()
+
+    if authenticatedtimestamp!="False":
+        return True
+    else:
+        return False
+
+# A static function to send a user to the authentication service.
+def sendForAuthentication():
+    logging.warning("----- 2 -----")
+    # Generate a unique ticket.
+    csticket = str(uuid.uuid1())
+
+    # Save the ticket so we can confirm the same user is returning from
+    # the authentication service.
+    session["csticket"] = csticket
+
+    # Send the user to the School of Computer Science's server to validate.
+    # Append to the url the GET parameters 'url' which tells the
+    # authentication service where to return and append the csticket which
+    # will be used to confirm that the same user is returning.
+    url = AUTHENTICATION_SERVICE_URL+"?url="+DEVELOPER_URL+"&csticket="+csticket
+
+    logging.warning(url+"\n\nredirecting\n")
+    return url
+
+
+# A static function to record that a user is authenticated.
+def recordAuthenticatedUser():
+    # Record the time authenticated.
+    session["authenticated"] = str(time.time())
+    logging.warning("----- authenticated timestamp "+session["authenticated"]+" -----")
+
+    # Record the user's username.
+    session["username"] = request.args["username"]
+
+    # Record the user's full name.
+    session["fullname"] = request.args["fullname"]
+
+    # Record the users's category.
+    session["usercategory"] = request.args["usercategory"]
+
+    # Record the users's department.
+    session["department"] = request.args["dept"]
+
+    # Record the users's study level. Note this is only available
+    # if the developer has called requireStudyLevel(). TODO: studylevel
+    # if (isset(request.args["studylevel"]))
+    #     session["studylevel"] = request.args["studylevel"]
+
+# A static function to get the timestamp when the user authenticated.
+# @return string
+def getTimeAuthenticated():
+    if session.get("authenticated"):
+        return session["authenticated"]
+    else:
+        return "False"
+
+# A static function to invalidate a user. This function will remove the
+# data from the global variable session
+def invalidateUser():
+    session.clear()
+    redirect(AUTHENTICATION_LOGOUT_URL)
+
+# A static function to reject a user who has failed to authenticate.
+def rejectUser():
+    logging.warning("----- reject -----")
+    # sys.exit("Authentication failure")
+    redirect("http://178.62.125.116:5000")
 
 @app.route('/logout')
 def logout():
-    session.pop('logged_in', None)
-    flash('You were logged out')
-    return redirect(url_for('show_root'))
+    # logging.info('---------- Logging out %s ----------', session['username'])
+    session.clear()
+    return redirect(AUTHENTICATION_LOGOUT_URL)
+
 
 if __name__ == '__main__':
-    # logging.basicConfig(filename='postcontent.log',level=logging.WARNING)
+    logging.basicConfig(level=logging.DEBUG)
     app.run(host='178.62.125.116')
